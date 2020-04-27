cdd71dda022500ccf9fa5ef31a1d62aee5609663
antalszava <antalszava@gmail.com>
2020-04-27 13:50:57 -0400

    Removing shebang

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env python3
 r"""Basic setup module"""
 from setuptools import setup
 
