dc0d42e7513b1818a678fe06e29e33073f70906c
antalszava <antalszava@gmail.com>
2020-04-27 14:32:15 -0400

    Small formatting changes

--- sqtom/fitting_2d.py
+++ sqtom/fitting_2d.py
@@ -15,7 +15,7 @@
 Twin-beam squeezer inverse-problem solver
 =========================================
 This module solves the *inverse* problem of given a joint photon number distribution find the best
-parameters describing different the quantum states in a twin-beam producing it.
+parameters describing the different quantum states in a twin-beam producing it.
 
 The ideas behind this module borrow are re-implementation of the ideas in
 
@@ -32,6 +32,7 @@ from sqtom.forward_solver import twinbeam_pmf
 def two_schmidt_mode_guess(jpd_data):
     """Given a two mode histogram, this function generates a "physically" motivated guess for the loss, Schmidt occupations
     and dark counts parameters.
+
     This model is sensible only if the average g2 of signal and idler is above 1.5.
 
     Args:
@@ -60,9 +61,10 @@ def two_schmidt_mode_guess(jpd_data):
 
 
 def marginal_calcs_2d(jpd_data, as_dict=True):
-    """ Given a two dimensional array of probabilities it calculates the first
+    """Given a two dimensional array of probabilities it calculates their first
     the mean photon numbers, their g2's and g11.
-    It returns these values as a dictionary or as an array
+
+    It returns these values as a dictionary or as an array.
 
     Args:
         jpd_data (array): probability mass function of the photon events
@@ -93,9 +95,11 @@ def marginal_calcs_2d(jpd_data, as_dict=True):
 
 def gen_hist_2d(beam1, beam2):
     """Calculate the joint probability mass function of events.
+
     Args:
         beam1 (array): 1D events array containing the raw click events of first beam
         beam2 (array): 1D events array containing the raw click events of second beam
+
     Returns:
         (array): probability mass function of the click patterns in vals.
     """
@@ -112,12 +116,14 @@ def fit_2d(
 ):
     """Takes as input the name of the model to fit to and the jpd of the data
     and returns the fitted model.
+
     Args:
         pd_data (array): one dimensional array of the probability distribution of the data
         guess (dict): dictionary with the guesses for the different parameters
         method (string): method to be used by the optimizer
         do_not_vary (list): list of variables that should be held constant during optimization
         cutoff (int): internal cutoff
+
     Returns:
         Object containing the optimized parameter and several goodness-of-fit statistics
     """
@@ -149,7 +155,8 @@ def fit_2d(
 
     def model_2d(params, jpd_data):
         (dim_s, dim_i) = pd_data.shape
-        return twinbeam_pmf(params, cutoff=cutoff)[:dim_s, :dim_i] - pd_data
+        relevant_joint_pmf = twinbeam_pmf(params, cutoff=cutoff)[:dim_s, :dim_i]
+        return relevant_joint_pmf - pd_data
 
     minner_model = Minimizer(model_2d, pars_model, fcn_args=([pd_data]))
     result_model = minner_model.minimize(method=method)
